016bf4623ce62c5f1257c071a4e4138ce837a3e5
abeeel <nabilaismiyatii@gmail.com>
2024-06-15 02:03:34 +0700

    revisi home pages

--- resources/views/template/navbar.blade.php
+++ resources/views/template/navbar.blade.php
@@ -117,7 +117,7 @@
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="{{ route('home') }}" class="nav-link active">Home</a>
+            <a href="{{ route('dashboard') }}" class="nav-link active">Home</a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
             <a href="contact.html" class="nav-link">Contact</a>
