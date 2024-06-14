de35d285beca67f0b672b1697ddf7d4bb6afc922
abeeel <nabilaismiyatii@gmail.com>
2024-06-14 20:23:38 +0700

    superadmin fitur, button edit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-white-pink overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
                 </div>
@@ -15,3 +15,16 @@
         </div>
     </div>
 </x-app-layout>
+
+<!-- Tambahkan ini di bagian akhir template jika Anda ingin mengintegrasikan CSS langsung di template ini -->
+<style>
+    .bg-white-pink {
+        background-color: #ffcccc;
+    }
+    .font-semibold {
+        color: #ff66b2; /* Warna pink */
+    }
+    .text-gray-900 {
+        color: #333333;
+    }
+</style>
